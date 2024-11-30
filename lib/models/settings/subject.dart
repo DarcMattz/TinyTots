3ca582d7a059f2ef480cc732d8ae58c36293985a
DarcMattz <jcarvajal_21UR0148@psu.edu.ph>
2024-11-30 19:10:44 +0800

    fix bug

--- lib/models/settings/subject.dart
+++ lib/models/settings/subject.dart
@@ -9,31 +9,3 @@ class Book {
     required this.lessons,
   });
 }
-
-List<Book> subjects = [
-  Book(
-    title: "All Aboard",
-    lessons: {
-      'Shapes': prefs.getInt('shapes_high_score') ?? 0,
-      'Alphabets': prefs.getInt('alphabets_high_score') ?? 0,
-    },
-  ),
-  Book(title: "Phonics", lessons: {
-    'Vowels': prefs.getInt('vowels_high_score') ?? 0,
-  }),
-  Book(title: "Science Health", lessons: {
-    'Body': prefs.getInt('body_high_score') ?? 0,
-    'Senses': prefs.getInt('senses_high_score') ?? 0,
-    'Care': prefs.getInt('care_high_score') ?? 0,
-  }),
-  Book(title: "Math", lessons: {
-    'Numbers': prefs.getInt('numbers_high_score') ?? 0,
-    'Add Subtract': prefs.getInt('add_subtract_high_score') ?? 0,
-  }),
-  Book(title: "Geography", lessons: {
-    'Abakada': prefs.getInt('abakada_high_score') ?? 0,
-    'Pamilya': prefs.getInt('pamilya_high_score') ?? 0,
-    'Kulay': prefs.getInt('kulay_high_score') ?? 0,
-    'Hugis': prefs.getInt('hugis_high_score') ?? 0,
-  }),
-];
--- lib/screens/settings/stats.dart
+++ lib/screens/settings/stats.dart
@@ -1,13 +1,19 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_application_1/components/utils/nice_button.dart';
 import 'package:flutter_application_1/gen/assets.gen.dart';
+import 'package:flutter_application_1/globals.dart';
 import 'package:flutter_application_1/models/settings/subject.dart';
 import 'package:flutter_application_1/screens/settings/stats_per_book.dart';
 import 'package:percent_indicator/circular_percent_indicator.dart';
 
-class StatsScreen extends StatelessWidget {
+class StatsScreen extends StatefulWidget {
   const StatsScreen({super.key});
 
+  @override
+  State<StatsScreen> createState() => _StatsScreenState();
+}
+
+class _StatsScreenState extends State<StatsScreen> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -37,6 +43,34 @@ class AndroidWelcome extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    List<Book> subjects = [
+      Book(
+        title: "All Aboard",
+        lessons: {
+          'Shapes': prefs.getInt('shapes_high_score') ?? 0,
+          'Alphabets': prefs.getInt('alphabets_high_score') ?? 0,
+        },
+      ),
+      Book(title: "Phonics", lessons: {
+        'Vowels': prefs.getInt('vowels_high_score') ?? 0,
+      }),
+      Book(title: "Science Health", lessons: {
+        'Body': prefs.getInt('body_high_score') ?? 0,
+        'Senses': prefs.getInt('senses_high_score') ?? 0,
+        'Care': prefs.getInt('care_high_score') ?? 0,
+      }),
+      Book(title: "Math", lessons: {
+        'Numbers': prefs.getInt('numbers_high_score') ?? 0,
+        'Add Subtract': prefs.getInt('add_subtract_high_score') ?? 0,
+      }),
+      Book(title: "Geography", lessons: {
+        'Abakada': prefs.getInt('abakada_high_score') ?? 0,
+        'Pamilya': prefs.getInt('pamilya_high_score') ?? 0,
+        'Kulay': prefs.getInt('kulay_high_score') ?? 0,
+        'Hugis': prefs.getInt('hugis_high_score') ?? 0,
+      }),
+    ];
+
     return Container(
       width: double.infinity,
       decoration: BoxDecoration(
